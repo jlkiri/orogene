2c50675c288dfebd11d30349d434d68ca6fc324d
jlkiri <interalia.dev@gmail.com>
2020-11-20 00:21:29 +0900

    Make it at least parseable

--- crates/oro-diagnostics/tests/derive_enum.rs
+++ crates/oro-diagnostics/tests/derive_enum.rs
@@ -29,17 +29,17 @@ pub enum Critical {
 
 #[test]
 fn it_works() {
-    let blue = Critical::Blue;
+    let blue = Critical::Blue(2);
     assert_eq!("Blue.", blue.advice().unwrap());
     assert_eq!("critical::blue", blue.label());
     assert_eq!(DiagnosticCategory::Misc, blue.category());
 
-    let red = Critical::Red;
+    let red = Critical::Red(2);
     assert_eq!("Red.", red.advice().unwrap());
     assert_eq!("critical::red", red.label());
     assert_eq!(DiagnosticCategory::Misc, red.category());
 
-    let orange = Critical::Orange;
+    let orange = Critical::Orange(3);
     assert_eq!("Orange.", orange.advice().unwrap());
     assert_eq!("critical::orange", orange.label());
     assert_eq!(DiagnosticCategory::Misc, orange.category());
